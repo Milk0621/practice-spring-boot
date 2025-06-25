0ef855c8db31262b5361814676419c0f0bbc9792
Milk0621 <wltjs5360@gmail.com>
2025-06-25 23:18:19 +0900

    feat: Thymeleaf 프래그먼트 기반 layout 적용
    - 공통 layout에 fragment 구조 도입
    - 각 페이지 content 영역 분리 및 동적 주입 구현

--- src/main/java/com/newlecture/web/controller/admin/board/NoticeController.java
+++ src/main/java/com/newlecture/web/controller/admin/board/NoticeController.java
@@ -1,6 +1,7 @@
 package com.newlecture.web.controller.admin.board;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,8 +10,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class NoticeController {
 	
 	@GetMapping("list")
-	public String list() {
-		return "admin/board/notice/list";
+	public String list(Model model) {
+		model.addAttribute("pageTitle", "공지사항");
+		model.addAttribute("content", "admin/board/notice/list :: content");
+		return "admin/inc/layout";
 	}
 
 	@GetMapping("detail")
