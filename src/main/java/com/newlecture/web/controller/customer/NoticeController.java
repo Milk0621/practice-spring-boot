7f0045016fdf69d932bd7e6bf53fdcc88502ccbc
Milk0621 <wltjs5360@gmail.com>
2025-06-25 20:21:48 +0900

    첫 커밋

---
+++ src/main/java/com/newlecture/web/controller/customer/NoticeController.java
@@ -0,0 +1,25 @@
+package com.newlecture.web.controller.customer;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//@RestController
+@Controller //view 페이지(jsp)를 넘겨줄때에는 Controller를 써야함
+@RequestMapping("/customer/notice/")
+public class NoticeController {
+	
+	@GetMapping("list")
+	public String list(Model model) {
+		
+		model.addAttribute("test", "hello~");
+		
+		return "customer/notice/list";
+	}
+	
+	@RequestMapping("detail")
+	public String detail() {
+		return "customer/notice/detail";
+	}
+}
