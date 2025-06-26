2e9954b484c2adb2afa250b4012ee817af7670da
Milk0621 <wltjs5360@gmail.com>
2025-06-26 14:30:47 +0900

    feat: 공지사항 목록 조회 기능 구현
    - Customer/Admin NoticeController에서 DAO 호출하여 데이터 조회
    - list.html에 공지사항 목록 출력용 th:each 반복문 추가

--- src/main/java/com/newlecture/web/controller/admin/board/NoticeController.java
+++ src/main/java/com/newlecture/web/controller/admin/board/NoticeController.java
@@ -1,18 +1,28 @@
 package com.newlecture.web.controller.admin.board;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.newlecture.web.entity.Notice;
+import com.newlecture.web.service.NoticeService;
+
 @Controller("adminNoticeController")
 @RequestMapping("/admin/board/notice/")
 public class NoticeController {
+	
+	@Autowired
+	private NoticeService service;
 	
 	@GetMapping("list")
 	public String list(Model model) {
 		model.addAttribute("pageTitle", "공지사항");
 		model.addAttribute("content", "admin/board/notice/list :: content");
+		List<Notice> list = service.getList();
 		return "admin/inc/layout";
 	}
 
@@ -20,6 +30,9 @@ public class NoticeController {
 	public String detail(Model model) {
 		model.addAttribute("pageTitle", "공지사항");
 		model.addAttribute("content", "admin/board/notice/detail :: content");
+		
+		Notice notice = service.get(1);
+				
 		return "admin/inc/layout";
 	}
 	
--- src/main/java/com/newlecture/web/controller/customer/NoticeController.java
+++ src/main/java/com/newlecture/web/controller/customer/NoticeController.java
@@ -1,20 +1,32 @@
 package com.newlecture.web.controller.customer;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.newlecture.web.entity.Notice;
+import com.newlecture.web.service.NoticeService;
 
 //@RestController
 @Controller //view 페이지(jsp)를 넘겨줄때에는 Controller를 써야함
 @RequestMapping("/customer/notice/")
 public class NoticeController {
 	
+	@Autowired
+	private NoticeService service;
+	
 	@GetMapping("list")
 	public String list(Model model) {
 		
+		List<Notice> list = service.getList();
+		
 		model.addAttribute("pageTitle", "공지사항");
 		model.addAttribute("content","customer/notice/list :: content");
+		model.addAttribute("list", list);
 		
 		return "customer/inc/layout";
 	}
